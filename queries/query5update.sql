a14ee52dc28efad97d114f34a42f9b606285a515
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-12-02 17:36:54 +0200

    Update query5update.sql

--- queries/query5update.sql
+++ queries/query5update.sql
@@ -1,6 +1,22 @@
--- Updates biography books to common rarity
-UPDATE book
+-- Number of shredded or burned books in 2023
+SELECT 
+    COUNT(*) AS books_disposed_count
+FROM (
+    SELECT 
+        book_id
+    FROM 
+        disposal
+    WHERE 
+        disposal_method = 'Shredding' 
+        AND EXTRACT(YEAR FROM date) = 2023
 
-SET rarity = 'Common'
+    UNION ALL
 
-WHERE genre = 'Biography';
+    SELECT 
+        book_id
+    FROM 
+        disposal
+    WHERE 
+        disposal_method = 'Burning' 
+        AND EXTRACT(YEAR FROM date) = 2023
+) disposed_books;
