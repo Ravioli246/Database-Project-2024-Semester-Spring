4eb57c3414fce0b8ace43a303837f258e2afb05b
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-12-18 12:18:42 +0200

    Update joined-query2.sql

--- joined-queries/joined-query2.sql
+++ joined-queries/joined-query2.sql
@@ -1,14 +1,11 @@
--- SQL join that reassigns all of Michaela's August 2024 upkeep
--- assignments to Traci Smith
-SELECT u.upkeep_id, 
-       u.book_id, 
-       b.title AS book_title, 
-       u.employee_id, 
-       e.name AS employee_name, 
-       u.reason_for_upkeep, 
-       u.date
-FROM public.upkeep u
-JOIN public.employee e ON u.employee_id = e.employee_id
-JOIN public.book b ON u.book_id = b.book_id
-WHERE e.name = 'Michaela Moore'
-  AND u.date BETWEEN '2024-08-01' AND '2024-08-31';
+-- Show disposed books but also additional information like
+-- employee_name, for example to create an extended chart
+SELECT b.title AS book_title, 
+       d.disposal_method, 
+       d.material_of_book, 
+       d.date AS disposal_date, 
+       e.name AS employee_name
+FROM public.disposal d
+JOIN public.book b ON d.book_id = b.book_id
+JOIN public.employee e ON d.employee_id = e.employee_id
+ORDER BY d.date DESC;
