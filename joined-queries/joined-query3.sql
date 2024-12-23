f496768a31dd73b3197c72f583930184abdb5398
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-12-23 18:23:44 +0200

    Update joined-query3.sql

--- joined-queries/joined-query3.sql
+++ joined-queries/joined-query3.sql
@@ -1,14 +1,9 @@
 -- SQL join that reassigns all of Michaela's August 2024 upkeep
 -- assignments to Traci Smith
-SELECT u.upkeep_id, 
-       u.book_id, 
-       b.title AS book_title, 
-       u.employee_id, 
-       e.name AS employee_name, 
-       u.reason_for_upkeep, 
-       u.date
-FROM public.upkeep u
-JOIN public.employee e ON u.employee_id = e.employee_id
-JOIN public.book b ON u.book_id = b.book_id
-WHERE e.name = 'Michaela Moore'
+UPDATE public.upkeep u
+SET employee_id = e2. employee_id
+FROM public. employee el
+JOIN public. employee e2 ON e2. name = 'Traci Smith'
+WHERE u. employee_id = el. employee_id
+  AND el. name = 'Michaela Moore'
   AND u.date BETWEEN '2024-08-01' AND '2024-08-31';
